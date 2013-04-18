12ab5b47df9f019b2fb83cc3cd2d4d615bfbdb4f
Mike Howe <dockter@almuramc.com>
2013-04-17 23:09:00 -0500

    Contributor!
    Signed-off-by: Mike Howe <dockter@almuramc.com>

--- src/main/java/com/almuradev/sprout/plugin/SproutListener.java
+++ src/main/java/com/almuradev/sprout/plugin/SproutListener.java
@@ -19,7 +19,38 @@
  */
 package com.almuradev.sprout.plugin;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.getspout.spout.block.SpoutCraftBlock;
+import org.getspout.spoutapi.block.SpoutBlock;
+import org.getspout.spoutapi.inventory.SpoutItemStack;
+import org.getspout.spoutapi.material.Block;
+import org.getspout.spoutapi.material.Material;
+import org.getspout.spoutapi.material.MaterialData;
+import org.getspout.spoutapi.material.block.GenericCustomBlock;
 
 public class SproutListener implements Listener {
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event){
+		Material material;		
+		Block sBlock = ((SpoutCraftBlock) event.getBlock()).getBlockType();		
+		if (sBlock instanceof GenericCustomBlock) {
+			GenericCustomBlock customBlock = (GenericCustomBlock) sBlock;	
+			
+			/*
+			if (customBlock.getNotchianName().equalsIgnoreCase("CEP_RPG_Florist.CEP_Florist_rosebush")) {
+				material = MaterialData.getMaterial("KFood_Core.strawberry");
+				event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new SpoutItemStack(material, 4));
+				SpoutBlock spoutBlock = (SpoutBlock) event.getBlock();
+				spoutBlock.setType(org.bukkit.Material.AIR);
+				spoutBlock.setTypeId(material.getRawId());
+				SpoutBlock myBlock = (SpoutBlock) MaterialData.getMaterial("Hi");
+				spoutBlock.setCustomBlock(null);
+				return;
+			}
+			*/
+		}
+	}
 }
