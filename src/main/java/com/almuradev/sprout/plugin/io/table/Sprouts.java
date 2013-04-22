f79f33c28c2e5cc5f3ffd6836fbe645f94ce83a9
Chris Sanders <zidane@outlook.com>
2013-04-22 18:54:23 -0500

    Finalize table structure.
    Just need to make the accessor.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/sprout/plugin/io/table/Sprouts.java
+++ src/main/java/com/almuradev/sprout/plugin/io/table/Sprouts.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Sprout.
+ *
+ * © 2013 AlmuraDev <http://www.almuradev.com/>
+ * Sprout is licensed under the GNU General Public License.
+ *
+ * Sprout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sprout is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU General Public License.
+ */
 package com.almuradev.sprout.plugin.io.table;
 
 import com.almuradev.sprout.api.crop.Sprout;
@@ -8,10 +27,19 @@ import com.alta189.simplesave.Table;
 @Table("sprouts")
 public class Sprouts {
 	@Id
-	private long key;
+	private int id;
 
 	@Field
 	private String world;
+
+	@Field
+	private int x;
+
+	@Field
+	private int y;
+
+	@Field
+	private int z;
 
 	@Field
 	private Sprout sprout;
@@ -20,28 +48,35 @@ public class Sprouts {
 
 	}
 
-	public Sprouts(String world, Sprout sprout) {
+	public Sprouts(String world, int x, int y, int z, Sprout sprout) {
 		this.world = world;
+		this.x = x;
+		this.y = y;
+		this.z = z;
 		this.sprout = sprout;
 	}
 
-	public long getKey() {
-		return key;
+	public int getId() {
+		return id;
 	}
 
 	public String getWorld() {
 		return world;
 	}
 
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
 	public Sprout getSprout() {
 		return sprout;
-	}
-
-	public void setSprout(Sprout sprout) {
-		this.sprout = sprout;
-	}
-
-	public void setWorld(String world) {
-		this.world = world;
 	}
 }
